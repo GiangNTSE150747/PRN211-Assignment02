6d3ee0e163ae3093475b5129f18f8bf226b6f178
Nguyen Truong Giang <GiangNTSE150747@fpt.edu.vn>
2022-06-30 14:39:46 +0700

    upload Business Layer + MemberDAO

---
+++ FStoreAppLibrary/Data Layer/Repository/MemberRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FStoreAppLibrary.Data_Layer.Repository
+{
+    internal class MemberRepository : IMemberRepository
+    {
+    }
+}
---
+++ TestConnect/Program.cs
@@ -0,0 +1,23 @@
+﻿using FStoreAppLibrary.Data_Layer;
+using FStoreAppLibrary.Models;
+using System.Collections;
+
+public class Program : FStoreContext
+{
+    public static void Main()
+    {
+        Member member = new Member
+        {
+            MemberId = 3,
+            Email = "Emxample@gmail.com",
+            CompanyName = "Fsoft",
+            City = "HCM",
+            Country = "Vietnam",
+            Password = "123"
+        };
+
+        Console.WriteLine(MemberDBContext.Instance.Insert(member));
+        Console.ReadLine();
+        
+    }
+}
